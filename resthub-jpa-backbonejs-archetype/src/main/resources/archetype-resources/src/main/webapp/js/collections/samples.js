e32d6de44159554d66a25a3cb2b88651c6283dac
Baptiste Meurant <baptiste.meurant@gmail.com>
2012-08-29 13:49:01 +0200

    change pagination API

--- resthub-jpa-backbonejs-archetype/src/main/resources/archetype-resources/src/main/webapp/js/collections/samples.js
+++ resthub-jpa-backbonejs-archetype/src/main/resources/archetype-resources/src/main/webapp/js/collections/samples.js
@@ -16,15 +16,12 @@ define(['resthub-backbone', 'backbone-paginator', 'models/sample'], function (Ba
         },
 
         paginator_ui:{
-            perPage:100 // Hardcoded for the moment
+            currentPage:'all'
         },
 
         server_api:{
             'page':function () {
                 return this.currentPage;
-            },
-            'perPage':function () {
-                return this.perPage;
             }
         },
 
