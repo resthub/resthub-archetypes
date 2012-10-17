71587721dca5e86dcab0c095faf6d4072d187765
Brian Clozel <brian.clozel@atos.net>
2012-10-17 18:39:19 +0200

    add draft for multi-modules jpa+backbonejs webapp.

---
+++ resthub-jpa-backbonejs-multi-archetype/src/main/resources/archetype-resources/__rootArtifactId__-contract/src/main/java/dto/Sample.java
@@ -0,0 +1,7 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.dto;
+
+public class Sample {
+}
---
+++ resthub-jpa-backbonejs-multi-archetype/src/main/resources/archetype-resources/__rootArtifactId__-core/src/main/java/service/SampleServiceImpl.java
@@ -0,0 +1,7 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.service;
+
+public class SampleServiceImpl implements SampleService {
+}
